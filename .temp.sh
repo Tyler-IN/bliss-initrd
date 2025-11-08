74b1f45d6d69c364e2746e846429d9adc678eead
Tyler Young <tyler.young@impromptu.ninja>
2025-11-08 07:14:24 -0500

    fix init

---
+++ .temp.sh
@@ -0,0 +1,5 @@
+mkdir ~/sda2 || echo already have sda2
+umount ~/sda2 || echo not mounted
+mount /dev/sda2 ~/sda2
+cp ~/sda2/android-2024-07-15/initrd_a.img.bkp ~/bliss-initrd/initrd.img
+find * | cpio -o -H newc | gzip -9 > ~/sda2/android-2024-07-15/initrd_a.img
